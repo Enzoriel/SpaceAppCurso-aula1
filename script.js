62b8f5cdedf14781ec4a8523cf9e4cb6d589cd29
Enzoriel <enzoknsb@outlook.com>
2024-06-08 17:13:50 -0300

    Reescribiendo el código desde cero

--- script.js
+++ script.js
@@ -3,19 +3,22 @@ const url = "https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&count=15";
 function listaImagenes() {
   fetch(url)
     .then((response) => response.json())
-    .then((datos) => {
-      console.log(datos);
-      const card = document.querySelector("[data-ul]");
-      datos.forEach((elemento) => {
-        const contenido = `<li class="card">
-          <img class="card__image" src="${elemento.url}" alt="imagen" />
-          <h3 class="card__title">${elemento.title}</h3>
+    .then((datosImagenes) => {
+      console.log(datosImagenes);
+
+      datosImagenes.forEach((element) => {
+        const card = document.querySelector("[data-ul]");
+
+        const contenido = ` <li class="card">
+          <img class="card__image" src="${element.url}" alt="imagen" />
+          <h3 class="card__title">${element.title}</h3>
         </li>
         `;
 
         card.innerHTML = card.innerHTML + contenido;
       });
     })
+
     .catch((error) => console.log(error));
 }
 
