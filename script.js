98d043f5c44df98b704c81c02ba9a5854ce106e4
Enzoriel <enzoknsb@outlook.com>
2024-06-10 11:02:01 -0300

    Utilizando async/await para las promises

--- script.js
+++ script.js
@@ -1,6 +1,31 @@
 const url = "https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&count=15";
 
-function listaImagenes() {
+// --> USANDO ==> async, try, await, catch
+
+async function listaImagenes() {
+  try {
+    let fetchImagenes = await fetch(url);
+    let datosImagenes = await fetchImagenes.json();
+
+    datosImagenes.forEach((element) => {
+      const card = document.querySelector("[data-ul]");
+
+      const contenido = ` <li class="card">
+        <img class="card__image" src="${element.url}" alt="imagen" />
+        <h3 class="card__title">${element.title}</h3>
+      </li>
+      `;
+
+      card.innerHTML = card.innerHTML + contenido;
+    });
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+// --> USANDO ==> then y catch
+
+/* function listaImagenes() {
   fetch(url)
     .then((response) => response.json())
     .then((datosImagenes) => {
@@ -20,6 +45,6 @@ function listaImagenes() {
     })
 
     .catch((error) => console.log(error));
-}
+} */
 
 listaImagenes();
